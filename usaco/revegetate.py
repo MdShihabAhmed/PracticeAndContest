b7f9603047476b421ac9cdaa173f4a6d94b368f1
shihab <talos@DESKTOP-25SG6HR.localdomain>
2022-11-19 17:30:34 +0600

    complex solution(just wanted to get it done)

---
+++ usaco/revegetate.py
@@ -0,0 +1,32 @@
+import sys
+
+def solver(filename):
+	sys.stdin = open(filename + '.in', 'r')
+	sys.stdout = open(filename + '.out', 'w')
+
+	#solve here
+	n, m = map(int,input().split())
+	graph = [[] for i in range(n+1)]
+	for i in range(m):
+		node1, node2 = map(int,input().split())
+		graph[node2].append(node1)
+		graph[node1].append(node2)
+
+	result = [0]*(n)
+	grass = {1,2,3,4}
+	for i in range(1,n+1):
+		if not result[i-1]:
+			result[i-1] = 1
+		for j in graph[i]:
+			temp = set()
+			for k in graph[j]:
+				if k<j:
+					temp.add(result[k-1])
+			temp = list(grass.difference(temp))
+			result[j-1] = min(list(temp))
+	
+	print(*result,sep='')
+
+
+if __name__ == "__main__":
+	solver('revegetate')
