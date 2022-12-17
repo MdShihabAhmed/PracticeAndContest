bba8d8362a1c937d52672fda8cbfd2dc352c224c
shihab <talos@DESKTOP-25SG6HR.localdomain>
2022-12-17 22:02:00 +0600

    similar to 1661

---
+++ usaco.guide/silver/cses_1662.py
@@ -0,0 +1,21 @@
+import sys
+from collections import defaultdict
+
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int,input().split()))
+
+prefixSum = [0]*(n+1)
+
+similar = defaultdict(int)
+similar[0] = 1
+
+result = 0
+for i in range(n):
+    prefixSum[i+1] = (prefixSum[i]+a[i])%n
+
+    result+= similar[prefixSum[i+1]]
+    similar[prefixSum[i+1]] += 1
+
+print(result)
